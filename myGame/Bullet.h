b91b3881ac4cbedb2a53de24c547527f4f4486d8
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-06-03 18:44:51 +0300

    Update Bullet.h

--- myGame/Bullet.h
+++ myGame/Bullet.h
@@ -3,7 +3,7 @@
 
 class Bullet {
 public:
-  Bullet(sf::Vector2f startPos, sf::Vector2f targetPos);
+  Bullet(sf::Vector2f startPos, sf::Vector2f targetPos, float scale = 0.5f, int damage = 100, float maxRange = 10000.f, const std::string& texturePath = "assets/bullet.gif");
 
   void update();
   void draw(sf::RenderWindow& window) const;
@@ -11,13 +11,17 @@ public:
 
   bool isActive;
 
+  int damage = 100;
+
 private:
   sf::Sprite sprite;
-  static sf::Texture bulletTexture;
-  static float speed;
+  float speed = 3.f;
 
   sf::Vector2f velocity;
   sf::Vector2f startPosition;
-  sf::Vector2f targetPosition;
-  float maxDistance;
+  float initialMaxDistance;
+
+  static std::map<std::string, sf::Texture> textureCache;
+
+  static const sf::Texture& getTexture(const std::string& path);
 };
