2ae43d84966bf229a97af3355c7449bf5c8731a1
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-06-03 18:42:54 +0300

    Update Upgrade.cpp

--- myGame/Upgrade.cpp
+++ myGame/Upgrade.cpp
@@ -5,22 +5,50 @@ void HealthUpgrade::apply(Player& player) {
     player.increaseMaxHealth(1.05f);
 }
 
+void MegaHealthUpgrade::apply(Player& player) {
+    player.increaseMaxHealth(1.20f);
+}
+
 void FireRateUpgrade::apply(Player& player) {
     player.decreaseShootDelay(0.95f);  // меньше задержка -> выше скорость
+}
+
+void RapidFireUpgrade::apply(Player& player) {
+    player.decreaseShootDelay(0.80f);
 }
 
 void SpeedBoostUpgrade::apply(Player& player) {
     player.increaseSpeed(1.05f);
 }
 
+void SprintBoostUpgrade::apply(Player& player) {
+    player.increaseSpeed(1.20f);
+}
+
 void HpRegenUpgrade::apply(Player& player) {
     player.enableHpRegen(1);
+}
+
+void AdvancedHpRegenUpgrade::apply(Player& player) {
+    player.enableHpRegen(3);
 }
 
 void VampirismUpgrade::apply(Player& player) {
     player.enableVampirism(1);
 }
 
+void GreaterVampirismUpgrade::apply(Player& player) {
+    player.enableVampirism(3);
+}
+
 void XPGainUpgrade::apply(Player& player) {
     player.increaseXPGainMultiplier(1.25f);
 }
+void XPSurgeUpgrade::apply(Player& player) {
+    player.increaseXPGainMultiplier(2.0f);
+}
+
+void ShieldUpgrade::apply(Player& player) {
+    player.enableShield();
+}
+
