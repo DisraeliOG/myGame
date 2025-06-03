f63aa4e606a7b621f31594aa8a652af8ef36fe44
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-06-03 18:42:19 +0300

    Update HUD.h

--- myGame/HUD.h
+++ myGame/HUD.h
@@ -3,6 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include "Player.h"
+#include "Boss.h"
 
 class HUD {
 private:
@@ -19,16 +20,31 @@ private:
 
     sf::Text levelText;
 
+    sf::RectangleShape shieldBarBack;
+    sf::RectangleShape shieldBarFront;
+
     bool showFinalTime = false;
+
+    // --- Босс-бар ---
+    sf::RectangleShape bossHpBarBack;
+    sf::RectangleShape bossHpBarFront;
+    sf::Text bossNameText;
+    float bossBarWidth = 600.f;
+    float bossBarHeight = 40.f;
+    float bossBarMargin = 70.f;
+    std::string currentBossName = "Hell Demon";
+    bool isBossBarVisible = false;
 
 public:
     HUD();
 
-    void update(const Player& player, const sf::Vector2u& windowSize, float elapsedTime);
+    void update(const Player& player, const sf::Vector2u& windowSize, float elapsedTime, const Boss* boss = nullptr);
     void draw(sf::RenderWindow& window);
 
     void resetFinalTime();
     std::string formatTime(float timeInSeconds);
+    const sf::Font& getFont() const;
+
 };
 
 #endif
